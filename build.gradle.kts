d8226a9d16480e446d77c86a3345688eaccd5fa5
Jovian12 <infernum212@gmail.com>
2026-02-20 00:28:01 +0700

    Fix build gradle v3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,9 @@
 plugins {
     java
-    checkstyle
-    jacoco
-    id("com.github.spotbugs") version "6.0.27"
-    id("org.springframework.boot") version "3.5.11"
+    id("org.springframework.boot") version "3.5.10"
     id("io.spring.dependency-management") version "1.1.7"
-    id("org.sonarqube") version "6.0.1.5171"
+    id("jacoco")
+    id("org.sonarqube") version "4.4.1.3373"
 }
 
 group = "id.ac.ui.cs.advprog"
@@ -28,43 +26,37 @@ repositories {
     mavenCentral()
 }
 
-checkstyle {
-    toolVersion = "10.17.0"
-}
-
-jacoco {
-    toolVersion = "0.8.12"
-}
-
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-mail")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
-
-    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
+    runtimeOnly("org.postgresql:postgresql")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     annotationProcessor("org.projectlombok:lombok")
-
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+    implementation("dev.samstevens.totp:totp:1.7.1")
+    testImplementation("com.h2database:h2")
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
 }
 
 tasks.jacocoTestReport {
     dependsOn(tasks.test)
     reports {
         xml.required.set(true)
-        html.required.set(true)
-    }
-}
-
-tasks.withType<com.github.spotbugs.snom.SpotBugsTask> {
-    reports.create("html") {
-        required.set(true)
-        outputLocation.set(layout.buildDirectory.file("reports/spotbugs/${name}.html"))
+        csv.required.set(false)
+        html.required.set(false)
     }
 }
