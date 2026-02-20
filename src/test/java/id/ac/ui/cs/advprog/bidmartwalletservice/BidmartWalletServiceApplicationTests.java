590c79b0481f33156603cf9234d37f0ce04b4fde
Jovian12 <infernum212@gmail.com>
2026-02-20 18:42:50 +0700

    Add unit test

--- src/test/java/id/ac/ui/cs/advprog/bidmartwalletservice/BidmartWalletServiceApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/bidmartwalletservice/BidmartWalletServiceApplicationTests.java
@@ -2,8 +2,16 @@ package id.ac.ui.cs.advprog.bidmartwalletservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.hibernate.ddl-auto=create-drop"
+})
 class BidmartWalletServiceApplicationTests {
 
     @Test
