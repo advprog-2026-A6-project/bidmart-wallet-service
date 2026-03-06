c111238cf2fb592bf1e04f0370c170831f11304f
Jovian12 <infernum212@gmail.com>
2026-03-06 16:31:40 +0700

    Fix some issues

--- src/main/java/id/ac/ui/cs/advprog/bidmartwalletservice/initializer/WalletInitializer.java
+++ src/main/java/id/ac/ui/cs/advprog/bidmartwalletservice/initializer/WalletInitializer.java
@@ -2,21 +2,22 @@ package id.ac.ui.cs.advprog.bidmartwalletservice.initializer;
 
 import id.ac.ui.cs.advprog.bidmartwalletservice.service.WalletService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j; // Tambahkan ini
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class WalletInitializer implements CommandLineRunner {
     private final WalletService walletService;
-
     @Override
     public void run(String... args) {
         try {
             walletService.getWalletByUserId(1L);
-            System.out.println(">>> Wallet for User 1 is ready!");
+            log.info(">>> Wallet for User 1 is ready!");
         } catch (Exception e) {
-            System.out.println(">>> Error initializing wallet: " + e.getMessage());
+            log.error(">>> Error initializing wallet: {}", e.getMessage());
         }
     }
 }
